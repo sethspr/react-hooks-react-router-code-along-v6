adcc4669d056a1ed16c6abafce6034de0c596790
seth spring <seth.c.spring@gmail.com>
2024-03-03 12:52:04 -0500

    completed router code along

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+// Step 1. Import react-router functions
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
